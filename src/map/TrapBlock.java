5fdb753ed3a5bd83a722724f964545510bba94a5
ktpunnisa <ktpunnisa@gmail.com>
2017-11-18 17:07:43 +0700

    add texture to block

--- src/map/TrapBlock.java
+++ src/map/TrapBlock.java
@@ -1,5 +1,28 @@
 package map;
 
-public class TrapBlock {
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 
+public class TrapBlock extends Block{
+	public TrapBlock(int x, int y, int c) {
+		super(x, y, c);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void loadImage() {
+		// TODO Auto-generated method stub
+		Image img = new Image("res/block/trap.png");
+		this.hexagon.setFill(new ImagePattern(img,0,0,1,1,true));
+		//this.hexagon.setFill(Color.ORANGE);
+		this.hexagon.setStrokeWidth(3);
+		this.hexagon.setStroke(Color.BLACK);
+	}
+
+	@Override
+	public void checkEvent() {
+		// TODO Auto-generated method stub
+		
+	}
 }
