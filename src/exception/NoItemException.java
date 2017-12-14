bef51c4936a89f0d153fc1950b6bda858d8146e6
Warit Prasartritha <doliolarzz@hotmail.com>
2017-12-14 15:21:28 +0700

    add exception

--- src/exception/NoItemException.java
+++ src/exception/NoItemException.java
@@ -3,5 +3,7 @@ package exception;
 public class NoItemException extends Exception {
 
 	private static final long serialVersionUID = 8125218660472803288L;
-
+	public NoItemException() {
+		System.out.println("No item");
+	}
 }
